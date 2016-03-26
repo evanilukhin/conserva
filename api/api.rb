d6bc1841d16585f5ac02d23fade667a73d4efea7
Иван Илюхин <mosk1995@gmail.com>
2016-03-26 13:21:14 +0300

    - реализованы заглушки для API; - загрузка файла; - обновлён gitignore.

--- api/api.rb
+++ api/api.rb
@@ -11,15 +11,22 @@ end
 
 # Запрос на конвертацию
 post '/convert_file' do
-  tempfile = params['file'][:tempfile]
-  filename = params['file'][:filename]
-  File.open("#{Time.now.strftime("%Y_%m_%d-%T")}_#{filename}",'wb') do |file|
+  # валидация запроса
+
+  # сохранение файла
+  tempfile = params.first[1][:tempfile]
+  filename = params.first[1][:filename]
+  File.open("../temp_files/#{Time.now.strftime("%Y_%m_%d-%T")}_#{filename}",'wb') do |file|
     file.write tempfile.read
   end
+
+  # создание задачи
   response.body = {message: "Task  successful created \n"}.to_json
 end
 
 # Запрос на получение готового файла
 get '/get_converted_file/:id_task' do
+  send_file '../README.md', filename: 'README'
   "Converted file from task with id #{params[:id_task]}"
+
 end
