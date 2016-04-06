e906b4c1b99ca53f1344aa656d21fadaae461133
ilyukhin <ilyukhin@elins.ru>
2016-04-06 10:14:49 +0300

    Добавлен модуль конвертации изображений (imagemagic).

--- example_convert_post.rb
+++ example_convert_post.rb
@@ -1,6 +1,6 @@
 require 'rest-client'
 RestClient.proxy = ''
 RestClient.post 'http://localhost:9292/convert_file',
-                input_extension: "dwg",
-                output_extension: "pdf",
-                file: File.new("test_files/test_4.dwg", 'rb')
+                input_extension: "png",
+                output_extension: "jpg",
+                file: File.new("test_files/test_3.png", 'rb')
---
+++ task_manager/convert_modules/image_magic_mod.rb
@@ -0,0 +1,26 @@
+class ImageConvert
+  class << self
+
+    def valid_combinations
+      {
+          from: %w(png),
+          to:   %w(jpg)
+      }
+    end
+
+    def run task
+        system "convert ../#{task.gotten_file_path} ../#{task.gotten_file_path.sub(/#{task.input_extension}$/i,
+                                                                                   task.output_extension)}"
+
+
+        task.state = 'finished'
+        task.updated_at = Time.now
+        task.converted_file_path = "../#{task.gotten_file_path.sub(/#{task.input_extension}$/i,
+                                                                   task.output_extension)}"
+        task.finished_at = Time.now
+        task.save
+    end
+  end
+end
+
+ConvertModulesLoader::ConvertModule.register ImageConvert
